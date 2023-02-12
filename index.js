a4c18040c45c33f64ff3213418f4fb1fdc7051b4
Danilo Correa <danilosilva87@gmail.com>
2023-02-12 18:19:01 -0300

    feat: new output added
    New output is separated into each version item

--- index.js
+++ index.js
@@ -5,21 +5,12 @@ async function run() {
   try {
     const tag     = core.getInput('tag');
     const semver  = new SemVer(tag);
-    var tagMajor  = `v${semver.major}`;
-    var tagRc     = `v${semver.major}.${semver.minor}.${semver.patch}`;
-    
-    if (semver.prerelease !== null) {
-      tagRc = `v${semver.major}.${semver.minor}.${semver.patch}-rc`
-    }
-
-    core.setOutput('tag_major', tagMajor);
-    core.debug(tagMajor)
-    core.setOutput('tag_rc', tagRc);
-    core.debug(tagRc)
-    core.setOutput('tag_rc_slug', tagRc.replaceAll('.', '-'));
-    core.debug(tagRc.replaceAll('.', '-'))
-    core.setOutput('tag_rc_underscore', tagRc.replaceAll('.', '_').replaceAll('-', '_'));
-    core.debug(tagRc.replaceAll('.', '_').replaceAll('-', '_'))
+    core.setOutput('raw', semver.raw);
+    core.setOutput('major', semver.major);
+    core.setOutput('minor', semver.minor);
+    core.setOutput('patch', semver.patch);
+    core.setOutput('release', semver.prerelease.length ? semver.prerelease[1] : null);
+    core.setOutput('version', semver.version);
 
   } catch (error) {
     core.setFailed(error.message);
